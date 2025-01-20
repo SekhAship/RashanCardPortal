09d3e6a4e20b4237b4bb9fffcac07283409c24e2
Sekh Aship <124449334+SekhAship@users.noreply.github.com>
2025-01-20 15:07:56 +0530

    Update firebaseConfig.jsx
    clean

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
 //apna firebase config dalo
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 const fireDB = getFirestore(app);
 const auth = getAuth(app);
 
 export { fireDB, auth }
-//////////////////////
+
